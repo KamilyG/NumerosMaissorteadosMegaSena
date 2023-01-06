9bde81e5cdf62858642e681f20d1ae4486907567
Kamily Grolli <kamily369@live.com>
2023-01-05 23:40:19 -0300

    Final commit

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 tabela = pd.read_excel("C:\\loteria\sorteios_loteria.xlsx")     #.loc[:, ['bola1', 'bola2', 'bola3', 'bola4', 'bola5', 'bola6']]
 tabela = tabela.values.tolist()
-num_mais_sorteados = {}
+num_sorteados = {}
 
-def calcular_quantidade():
-    for l in range(len(tabela)):
-        for n in tabela[l]:
-            if n in num_mais_sorteados:
-                num_mais_sorteados[n] += 1
-            else:
-                num_mais_sorteados[n] = 1
+for linha in range(len(tabela)): # conta a quantidade de vezes que um número apareceu e o adiciona ao dicionário aumentando sua quantidade
+    for numero in tabela[linha]:
+        if numero in num_sorteados:
+            num_sorteados[numero] += 1
+        else:
+            num_sorteados[numero] = 1
 
-print(num_mais_sorteados)
+for i in sorted(num_sorteados, key = num_sorteados.get, reverse=True): # printa os dados do dicionários de forma decrescente
+    print("O número {} foi sorteado {} vez(es)." .format(i, num_sorteados[i]))
